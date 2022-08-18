22ec3555edb7279e5d0fa928b185d9af10073281
Sandor <41598015+sandorprojects@users.noreply.github.com>
2022-08-17 18:08:13 -0700

    Updated for RPI and socket integration
    The scripts have been updated to accommodate the RPI live data feed

--- src/Socket Communication/Client.py
+++ src/Socket Communication/Client.py
@@ -13,12 +13,12 @@ class nowServingClient:
 
     # For the client to make a connection with the server -> server.py
     def __init__(self):
-        size = 11
+        size = 4
         self.battery_id1 = [0] * size
         self.battery_id2 = [0] * size
         self.battery_id3 = [0] * size
         self.battery_id4 = [0] * size
-        self.battery_id5 = [0] * size
+        # self.battery_id5 = [0] * size
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as active_socket:
             print('Waiting for connection')
@@ -48,161 +48,158 @@ class nowServingClient:
             # So, the data will need to be split. As it is split it is automatically put into an array and store in "array_Element"
             array_Element = decrypted_Text.split(" ")
 
-            # # Splitting the data from "array_Element" into 0-10 and 11-21 to be stored in "battery_id1" and "battery_id2"
-            for i in range(55):
-                if i <= 10:
-                    self.battery_id1[i] = array_Element[i]  # storing elements 0-10 of "array_Element" in "battery_id1"
-                elif 11 <= i <= 21:
-                    self.battery_id2[i - 11] = array_Element[
-                        i]  # storing elements 11-21 of "array_Element" in "battery_id2"
-                elif 22 <= i <= 32:
-                    self.battery_id3[i - 22] = array_Element[
-                        i]  # storing elements 22-32 of "array_Element" in "battery_id3"
-                elif 33 <= i <= 43:
-                    self.battery_id4[i - 33] = array_Element[
-                        i]  # storing elements 33-43 of "array_Element" in "battery_id4"
-                elif 44 <= i <= 54:
-                    self.battery_id5[i - 44] = array_Element[
-                        i]  # storing elements 44-54 of "array_Element" in "battery_id5"
+            # Splitting the data from "array_Element" into 0-10 and 11-21 to be stored in "battery_id1" and "battery_id2"
+            # for i in range(55):
+            #     if i <= 10:
+            #         self.battery_id1[i] = array_Element[i]  # storing elements 0-10 of "array_Element" in "battery_id1"
+            #     elif 11 <= i <= 21:
+            #         self.battery_id2[i - 11] = array_Element[
+            #             i]  # storing elements 11-21 of "array_Element" in "battery_id2"
+            #     elif 22 <= i <= 32:
+            #         self.battery_id3[i - 22] = array_Element[
+            #             i]  # storing elements 22-32 of "array_Element" in "battery_id3"
+            #     elif 33 <= i <= 43:
+            #         self.battery_id4[i - 33] = array_Element[
+            #             i]  # storing elements 33-43 of "array_Element" in "battery_id4"
+            #     elif 44 <= i <= 54:
+            #         self.battery_id5[i - 44] = array_Element[
+            #             i]  # storing elements 44-54 of "array_Element" in "battery_id5"
+
+            for i in range(16):
+                if i <= 3:
+                    self.battery_id1[i] = array_Element[i]  # storing elements 0-3 of "array_Element" in "battery_id1"
+                elif 4 <= i <= 7:
+                    self.battery_id2[i - 4] = array_Element[i]  # storing elements 4-7 of "array_Element" in "battery_id2"
+                elif 8 <= i <= 11:
+                    self.battery_id3[i - 8] = array_Element[i]  # storing elements 8-11 of "array_Element" in "battery_id3"
+                elif 12 <= i <= 15:
+                    self.battery_id4[i - 12] = array_Element[i]  # storing elements 12-15 of "array_Element" in "battery_id4"
 
             # Printing the data to the terminal
             # Battery ID 1
             print("~~~~~~~~~~~~PRINTING BATTERY ID 1~~~~~~~~~~~~")
-            print("Received: ID -> #", self.battery_id1[0])
-            print("Received: Capacity -> ", self.battery_id1[1], "%")
-            print("Received: Energy ->", self.battery_id1[2], "wH")
-            print("Received: Charge Capacity ->", self.battery_id1[3], "mAh")
-            print("Received: Temperature ->", self.battery_id1[4], "C")
-            print("Received: BMS Health ->", self.battery_id1[5], "%")
-            print("Received: Total Voltage ->", self.battery_id1[6], "V")
-            print("Received: Cell 1 Voltage ->", self.battery_id1[7], "V")
-            print("Received: Cell 2 Voltage ->", self.battery_id1[8], "V")
-            print("Received: Cell 3 Voltage ->", self.battery_id1[9], "V")
-            print("Received: Cell 4 Voltage->", self.battery_id1[10], "V")
+            # print("Received: ID -> #", self.battery_id1[0])
+            # print("Received: Capacity -> ", self.battery_id1[1], "%")
+            # print("Received: Energy ->", self.battery_id1[2], "wH")
+            # print("Received: Charge Capacity ->", self.battery_id1[3], "mAh")
+            # print("Received: Temperature ->", self.battery_id1[4], "C")
+            # print("Received: BMS Health ->", self.battery_id1[5], "%")
+            # print("Received: Total Voltage ->", self.battery_id1[6], "V")
+            print("Received: Cell 1 Voltage ->", self.battery_id1[0], "V")
+            print("Received: Cell 2 Voltage ->", self.battery_id1[1], "V")
+            print("Received: Cell 3 Voltage ->", self.battery_id1[2], "V")
+            print("Received: Cell 4 Voltage->", self.battery_id1[3], "V")
 
             # Battery ID 2
             print("~~~~~~~~~~~~PRINTING BATTERY ID 2~~~~~~~~~~~~")
-            print("Received: ID -> #", self.battery_id2[0])
-            print("Received: Capacity -> ", self.battery_id2[1], "%")
-            print("Received: Energy ->", self.battery_id2[2], "wH")
-            print("Received: Charge Capacity ->", self.battery_id2[3], "mAh")
-            print("Received: Temperature ->", self.battery_id2[4], "C")
-            print("Received: BMS Health ->", self.battery_id2[5], "%")
-            print("Received: Total Voltage ->", self.battery_id2[6], "V")
-            print("Received: Cell 1 Voltage ->", self.battery_id2[7], "V")
-            print("Received: Cell 2 Voltage ->", self.battery_id2[8], "V")
-            print("Received: Cell 3 Voltage ->", self.battery_id2[9], "V")
-            print("Received: Cell 4 Voltage->", self.battery_id2[10], "V")
+            # print("Received: ID -> #", self.battery_id2[0])
+            # print("Received: Capacity -> ", self.battery_id2[1], "%")
+            # print("Received: Energy ->", self.battery_id2[2], "wH")
+            # print("Received: Charge Capacity ->", self.battery_id2[3], "mAh")
+            # print("Received: Temperature ->", self.battery_id2[4], "C")
+            # print("Received: BMS Health ->", self.battery_id2[5], "%")
+            # print("Received: Total Voltage ->", self.battery_id2[6], "V")
+            print("Received: Cell 1 Voltage ->", self.battery_id2[0], "V")
+            print("Received: Cell 2 Voltage ->", self.battery_id2[1], "V")
+            print("Received: Cell 3 Voltage ->", self.battery_id2[2], "V")
+            print("Received: Cell 4 Voltage->", self.battery_id2[3], "V")
 
             # Battery ID 3
             print("~~~~~~~~~~~~PRINTING BATTERY ID 3~~~~~~~~~~~~")
-            print("Received: ID -> #", self.battery_id3[0])
-            print("Received: Capacity -> ", self.battery_id3[1], "%")
-            print("Received: Energy ->", self.battery_id3[2], "wH")
-            print("Received: Charge Capacity ->", self.battery_id3[3], "mAh")
-            print("Received: Temperature ->", self.battery_id3[4], "C")
-            print("Received: BMS Health ->", self.battery_id3[5], "%")
-            print("Received: Total Voltage ->", self.battery_id3[6], "V")
-            print("Received: Cell 1 Voltage ->", self.battery_id3[7], "V")
-            print("Received: Cell 2 Voltage ->", self.battery_id3[8], "V")
-            print("Received: Cell 3 Voltage ->", self.battery_id3[9], "V")
-            print("Received: Cell 4 Voltage->", self.battery_id3[10], "V")
+            # print("Received: ID -> #", self.battery_id3[0])
+            # print("Received: Capacity -> ", self.battery_id3[1], "%")
+            # print("Received: Energy ->", self.battery_id3[2], "wH")
+            # print("Received: Charge Capacity ->", self.battery_id3[3], "mAh")
+            # print("Received: Temperature ->", self.battery_id3[4], "C")
+            # print("Received: BMS Health ->", self.battery_id3[5], "%")
+            # print("Received: Total Voltage ->", self.battery_id3[6], "V")
+            print("Received: Cell 1 Voltage ->", self.battery_id3[0], "V")
+            print("Received: Cell 2 Voltage ->", self.battery_id3[1], "V")
+            print("Received: Cell 3 Voltage ->", self.battery_id3[2], "V")
+            print("Received: Cell 4 Voltage->", self.battery_id3[3], "V")
 
             # Battery ID 4
             print("~~~~~~~~~~~~PRINTING BATTERY ID 4~~~~~~~~~~~~")
-            print("Received: ID -> #", self.battery_id4[0])
-            print("Received: Capacity -> ", self.battery_id4[1], "%")
-            print("Received: Energy ->", self.battery_id4[2], "wH")
-            print("Received: Charge Capacity ->", self.battery_id4[3], "mAh")
-            print("Received: Temperature ->", self.battery_id4[4], "C")
-            print("Received: BMS Health ->", self.battery_id4[5], "%")
-            print("Received: Total Voltage ->", self.battery_id4[6], "V")
-            print("Received: Cell 1 Voltage ->", self.battery_id4[7], "V")
-            print("Received: Cell 2 Voltage ->", self.battery_id4[8], "V")
-            print("Received: Cell 3 Voltage ->", self.battery_id4[9], "V")
-            print("Received: Cell 4 Voltage->", self.battery_id4[10], "V")
+            # print("Received: ID -> #", self.battery_id4[0])
+            # print("Received: Capacity -> ", self.battery_id4[1], "%")
+            # print("Received: Energy ->", self.battery_id4[2], "wH")
+            # print("Received: Charge Capacity ->", self.battery_id4[3], "mAh")
+            # print("Received: Temperature ->", self.battery_id4[4], "C")
+            # print("Received: BMS Health ->", self.battery_id4[5], "%")
+            # print("Received: Total Voltage ->", self.battery_id4[6], "V")
+            print("Received: Cell 1 Voltage ->", self.battery_id4[0], "V")
+            print("Received: Cell 2 Voltage ->", self.battery_id4[1], "V")
+            print("Received: Cell 3 Voltage ->", self.battery_id4[2], "V")
+            print("Received: Cell 4 Voltage->", self.battery_id4[3], "V")
 
-            # Battery ID 5
-            print("~~~~~~~~~~~~PRINTING BATTERY ID 5~~~~~~~~~~~~")
-            print("Received: ID -> #", self.battery_id5[0])
-            print("Received: Capacity -> ", self.battery_id5[1], "%")
-            print("Received: Energy ->", self.battery_id5[2], "wH")
-            print("Received: Charge Capacity ->", self.battery_id5[3], "mAh")
-            print("Received: Temperature ->", self.battery_id5[4], "C")
-            print("Received: BMS Health ->", self.battery_id5[5], "%")
-            print("Received: Total Voltage ->", self.battery_id5[6], "V")
-            print("Received: Cell 1 Voltage ->", self.battery_id5[7], "V")
-            print("Received: Cell 2 Voltage ->", self.battery_id5[8], "V")
-            print("Received: Cell 3 Voltage ->", self.battery_id5[9], "V")
-            print("Received: Cell 4 Voltage->", self.battery_id5[10], "V")
+            # # Battery ID 5
+            # print("~~~~~~~~~~~~PRINTING BATTERY ID 5~~~~~~~~~~~~")
+            # print("Received: ID -> #", self.battery_id5[0])
+            # print("Received: Capacity -> ", self.battery_id5[1], "%")
+            # print("Received: Energy ->", self.battery_id5[2], "wH")
+            # print("Received: Charge Capacity ->", self.battery_id5[3], "mAh")
+            # print("Received: Temperature ->", self.battery_id5[4], "C")
+            # print("Received: BMS Health ->", self.battery_id5[5], "%")
+            # print("Received: Total Voltage ->", self.battery_id5[6], "V")
+            # print("Received: Cell 1 Voltage ->", self.battery_id5[7], "V")
+            # print("Received: Cell 2 Voltage ->", self.battery_id5[8], "V")
+            # print("Received: Cell 3 Voltage ->", self.battery_id5[9], "V")
+            # print("Received: Cell 4 Voltage->", self.battery_id5[10], "V")
 
-            # creating a diction to implement a JSON dump into a file
+            # creating a dictionary to implement a JSON dump into a file
             dictionary = {
                 "Battery Data": [
                     {
-                        "ID": self.battery_id1[0],
-                        "Capacity": self.battery_id1[1],
-                        "Energy": self.battery_id1[2],
-                        "Charge Capacity": self.battery_id1[3],
-                        "Temperature": self.battery_id1[4],
-                        "BMS Health": self.battery_id1[5],
-                        "Total Voltage": self.battery_id1[6],
-                        "Cell 1 Voltage": self.battery_id1[7],
-                        "Cell 2 Voltage": self.battery_id1[8],
-                        "Cell 3 Voltage": self.battery_id1[9],
-                        "Cell 4 Voltage": self.battery_id1[10]
+                        # "ID": self.battery_id1[0],
+                        # "Capacity": self.battery_id1[1],
+                        # "Energy": self.battery_id1[2],
+                        # "Charge Capacity": self.battery_id1[3],
+                        # "Temperature": self.battery_id1[4],
+                        # "BMS Health": self.battery_id1[5],
+                        # "Total Voltage": self.battery_id1[6],
+                        "Cell 1 Voltage": self.battery_id1[0],
+                        "Cell 2 Voltage": self.battery_id1[1],
+                        "Cell 3 Voltage": self.battery_id1[2],
+                        "Cell 4 Voltage": self.battery_id1[3]
                     },
                     {
-                        "ID": self.battery_id2[0],
-                        "Capacity": self.battery_id2[1],
-                        "Energy": self.battery_id2[2],
-                        "Charge Capacity": self.battery_id2[3],
-                        "Temperature": self.battery_id2[4],
-                        "BMS Health": self.battery_id2[5],
-                        "Total Voltage": self.battery_id2[6],
-                        "Cell 1 Voltage": self.battery_id2[7],
-                        "Cell 2 Voltage": self.battery_id2[8],
-                        "Cell 3 Voltage": self.battery_id2[9],
-                        "Cell 4 Voltage": self.battery_id2[10]
+                        # "ID": self.battery_id2[0],
+                        # "Capacity": self.battery_id2[1],
+                        # "Energy": self.battery_id2[2],
+                        # "Charge Capacity": self.battery_id2[3],
+                        # "Temperature": self.battery_id2[4],
+                        # "BMS Health": self.battery_id2[5],
+                        # "Total Voltage": self.battery_id2[6],
+                        "Cell 1 Voltage": self.battery_id2[0],
+                        "Cell 2 Voltage": self.battery_id2[1],
+                        "Cell 3 Voltage": self.battery_id2[2],
+                        "Cell 4 Voltage": self.battery_id2[3]
                     },
                     {
-                        "ID": self.battery_id3[0],
-                        "Capacity": self.battery_id3[1],
-                        "Energy": self.battery_id3[2],
-                        "Charge Capacity": self.battery_id3[3],
-                        "Temperature": self.battery_id3[4],
-                        "BMS Health": self.battery_id3[5],
-                        "Total Voltage": self.battery_id3[6],
-                        "Cell 1 Voltage": self.battery_id3[7],
-                        "Cell 2 Voltage": self.battery_id3[8],
-                        "Cell 3 Voltage": self.battery_id3[9],
-                        "Cell 4 Voltage": self.battery_id3[10]
+                        # "ID": self.battery_id3[0],
+                        # "Capacity": self.battery_id3[1],
+                        # "Energy": self.battery_id3[2],
+                        # "Charge Capacity": self.battery_id3[3],
+                        # "Temperature": self.battery_id3[4],
+                        # "BMS Health": self.battery_id3[5],
+                        # "Total Voltage": self.battery_id3[6],
+                        "Cell 1 Voltage": self.battery_id3[0],
+                        "Cell 2 Voltage": self.battery_id3[1],
+                        "Cell 3 Voltage": self.battery_id3[2],
+                        "Cell 4 Voltage": self.battery_id3[3]
                     },
                     {
-                        "ID": self.battery_id4[0],
-                        "Capacity": self.battery_id4[1],
-                        "Energy": self.battery_id4[2],
-                        "Charge Capacity": self.battery_id4[3],
-                        "Temperature": self.battery_id4[4],
-                        "BMS Health": self.battery_id4[5],
-                        "Total Voltage": self.battery_id4[6],
-                        "Cell 1 Voltage": self.battery_id4[7],
-                        "Cell 2 Voltage": self.battery_id4[8],
-                        "Cell 3 Voltage": self.battery_id4[9],
-                        "Cell 4 Voltage": self.battery_id4[10]
-                    },
-                    {
-                        "ID": self.battery_id5[0],
-                        "Capacity": self.battery_id5[1],
-                        "Energy": self.battery_id5[2],
-                        "Charge Capacity": self.battery_id5[3],
-                        "Temperature": self.battery_id5[4],
-                        "BMS Health": self.battery_id5[5],
-                        "Total Voltage": self.battery_id5[6],
-                        "Cell 1 Voltage": self.battery_id5[7],
-                        "Cell 2 Voltage": self.battery_id5[8],
-                        "Cell 3 Voltage": self.battery_id5[9],
-                        "Cell 4 Voltage": self.battery_id5[10]
+                        # "ID": self.battery_id4[0],
+                        # "Capacity": self.battery_id4[1],
+                        # "Energy": self.battery_id4[2],
+                        # "Charge Capacity": self.battery_id4[3],
+                        # "Temperature": self.battery_id4[4],
+                        # "BMS Health": self.battery_id4[5],
+                        # "Total Voltage": self.battery_id4[6],
+                        "Cell 1 Voltage": self.battery_id4[0],
+                        "Cell 2 Voltage": self.battery_id4[1],
+                        "Cell 3 Voltage": self.battery_id4[2],
+                        "Cell 4 Voltage": self.battery_id4[3]
                     }
                 ]
             }
--- src/Socket Communication/server.py
+++ src/Socket Communication/server.py
@@ -8,6 +8,7 @@ from time import sleep
 # These are calling on user made python scripts
 import extractJSONFile
 import AES_Security
+import GetBMSData
 
 HOST = "127.0.0.1"  # Standard loop back interface address (local host)
 PORT = 65434  # Port to listen on (non-privileged port are > 1023)
@@ -44,6 +45,7 @@ def client_handler(connection):
         print(f"Connected by {connection}")
 
         while True:
+            # GetBMSData.main()
             byte_Data = extractJSONFile.retrieve_Data().decode('UTF-8')  # calling a method from a different script to get the data from the JSON file and decoding it into a string
             myKey = "EncrYption KEy!!"  # Using this variable for the encryption key to pass on to the security script
             encrypted_byte_Data = AES_Security.AESCipher(myKey).encrypt(byte_Data)  # calling the script "security" to encrypt "byte_Data"
@@ -51,7 +53,7 @@ def client_handler(connection):
             print("Encrypted data ready for the server to send: ", encrypted_byte_Data)
             connection.send(encrypted_byte_Data.encode('UTF-8'))  # sending the encrypted text as well as encoding it into byte format since sockets can only transfer as byte format
 
-            sleep(1)  # in order for the server to be in sync with the client while sending data continuously, a time delay of 1 second is added
+            sleep(3)  # in order for the server to be in sync with the client while sending data continuously, a time delay of 1 second is added
 
             # break out of while loop and close the server
             loopBreak = 0
@@ -61,3 +63,4 @@ def client_handler(connection):
 
 if __name__ == '__main__':
     run_Server(HOST, PORT)
+
